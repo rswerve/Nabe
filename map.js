ebbf8eccc563231c5653a8e41cb498a65ee4cf0d
rswerve <rswerve@gmail.com>
2016-03-15 19:46:39 -0500

    messing with layers

--- map.js
+++ map.js
@@ -5,12 +5,15 @@ var map = new mapboxgl.Map({
     style: 'mapbox://styles/atighi/cilocmidf000s9mkmkmiyqc6v', //stylesheet location
     center: [-88.89, 41.7], // starting position
     zoom: 7 // starting zoom
+
 });
 
 map.addControl(new mapboxgl.Navigation({position: 'bottom-left'})); // no position = top-right
-
 //get lat/lng from click and send those coordinates to tracts route in server.js
 map.on('click', function(data) {
+  map.style._layers["acs2014-5yr-b15003-14000us170318"].layout.visibility = "none"
+  console.log('zoom ', map.style.z)
+  console.log(map)
   var lngLat = {lng: data.lngLat.lng, lat:data.lngLat.lat}
   $.ajax({
     url: '/tracts',
@@ -22,3 +25,9 @@ map.on('click', function(data) {
     }
   })
 });
+
+map.on('dblclick', function(e){
+  console.log('e ', e)
+  console.log('doubleclick ', map)
+  map.setLayoutProperty ("acs2014-5yr-b15003-14000us170318", 'visibility', 'none')
+})
