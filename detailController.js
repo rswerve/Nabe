e0950575e19426fd571ac7b83755e8293c8ceb84
rswerve <rswerve@gmail.com>
2016-04-04 15:33:51 -0500

    handle towns

--- detailController.js
+++ detailController.js
@@ -28,6 +28,7 @@ myApp.controller('detailController',  function($scope, detailFactory, sideFactor
     detailFactory.locale(instaCoords)
     .then(function(data){
       $scope.locale = data.data.address
+      console.log($scope.locale)
     })
   }
 
